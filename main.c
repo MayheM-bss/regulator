fe3a2048490593f126e2c2d6490cf4d1ae9fa9a0
MayheM-bss <mayhembss@gmail.com>
2020-05-28 02:01:23 +0400

    changed project from c++ to c

---
+++ main.c
@@ -0,0 +1,61 @@
+/*
+ * regulator.c
+ *
+ * Created: 28.05.2020 1:18:42
+ * Author : Aleksey
+ */ 
+#define F_CPU 8000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+
+ISR (TIMER0_OVF_vect)
+{
+	
+}
+
+ISR (USART0_RX_vect)
+{
+	
+}
+
+ISR (INT0_vect)
+{
+	
+}
+
+void TimerInit()
+{
+	
+}
+
+void UARTInit()
+{
+	
+}
+
+void SPIInit()
+{
+	
+}
+
+void PWMInit()
+{
+	
+}
+
+
+
+int main(void)
+{
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+	
+    }
+}
+
