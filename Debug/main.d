f91c7ec031c46d5ae57156ca7d2350852f2a3790
MayheM-bss <mayhembss@gmail.com>
2020-06-03 23:28:53 +0400

    Configurate PWM, timer, usart

--- Debug/main.d
+++ Debug/main.d
@@ -16,7 +16,8 @@ main.d main.o: .././main.c \
  d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h
 
 d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -53,3 +54,5 @@ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
 
 d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
--- main.c
+++ main.c
@@ -10,31 +10,56 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
+#include <string.h>
 
+const float dt = 0.035;
+int reqTemp;
+
+unsigned char USARTRecevied()
+{
+	while(!(UCSR0A & (1<<RXC0)));
+	return UDR0;
+}
 
 ISR (TIMER0_OVF_vect)
 {
+	cli();
 	
+	sei();
 }
 
 ISR (USART0_RX_vect)
 {
-	
+	cli();
+	unsigned char message[5];
+	for (int i = 0; i < 5; i++)
+	{
+		message[i] = USARTRecevied();
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		reqTemp = reqTemp + message[i];
+	}
+	sei();
 }
 
-ISR (INT0_vect)
-{
-	
-}
 
 void TimerInit()
 {
-	
+	TCCR0 = 0x07;
+	TCNT0 = 0;
+	TIMSK |= (1<<TOIE0);
+	sei();
 }
 
 void UARTInit()
 {
-	
+	int speed = 52;
+	UCSR0A = 0;
+	UCSR0B = 0x98;
+	UCSR0C = 0x06;
+	UBRR0H = (unsigned char)(speed >> 8);
+	UBRR0L = (unsigned char)(speed);
 }
 
 void SPIInit()
@@ -44,13 +69,44 @@ void SPIInit()
 
 void PWMInit()
 {
-	
+	TCCR1A = 0x83;
+	TCCR1B = 0x09;
+	TCNT1H = 0x00;
+	TCNT1L = 0x00;
+}
+
+void USARTTransmitted(char str[])
+{
+	register char i = 0;
+	while(str[i])
+	{
+		while(!( UCSR0A & (1<<UDRE0)))
+		{
+			UDR0 = str[i++];
+		}
+	}
+	while(!( UCSR0A & (1<<UDRE0)))
+	{
+		UDR0 = ((char)13);
+	}
+	while(!( UCSR0A & (1<<UDRE0)))
+	{
+		UDR0 = ((char)10);
+	}
 }
 
 
+void SetPWM()
+{
+	
+}
 
 int main(void)
 {
+	SPIInit();
+	UARTInit();
+	PWMInit();
+	TimerInit();
 	
     /* Replace with your application code */
     while (1) 
